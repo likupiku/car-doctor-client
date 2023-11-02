e6236835c688606591471a63f4fdfb2268790525
likupiku <emon.ahmedliku@gmail.com>
2023-11-03 00:15:55 +0600

    token done

--- src/Pages/Bookings/Bookings.jsx
+++ src/Pages/Bookings/Bookings.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../Providers/AuthProvider";
 import BookingTable from "./BookingTable";
+import axios from "axios";
 
 const Bookings = () => {
     const [bookings, setBookings] = useState([])
@@ -8,11 +9,15 @@ const Bookings = () => {
     const url = `http://localhost:5000/bookings?email=${user?.email}`
 
     useEffect(() => {
-        fetch(url)
-            .then(res => res.json())
-            .then(data => {
-                setBookings(data)
+        axios.get(url, { withCredentials: true })
+            .then(res => {
+                setBookings(res.data)
             })
+        // fetch(url)
+        //     .then(res => res.json())
+        //     .then(data => {
+        //         setBookings(data)
+        //     })
     }, [url])
 
     const handledelete = id => {
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,9 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import login from "../../assets/images/login/login.svg"
 import { useContext } from "react";
 import { AuthContext } from "../../Providers/AuthProvider";
+import axios from "axios";
 const Login = () => {
     const { signIn } = useContext(AuthContext)
+    const location = useLocation()
+    const navigate = useNavigate()
+    console.log(location)
 
     const handleLogIn = e => {
         e.preventDefault();
@@ -14,8 +18,20 @@ const Login = () => {
         // user sign in
         signIn(email, password)
             .then(result => {
-                const user = result.user;
-                console.log(user)
+                const loggedUser = result.user;
+                console.log(loggedUser);
+                const user = { email }
+
+                // get access token
+                axios.post('http://localhost:5000/jwt', user, {
+                    withCredentials: true,
+                })
+                    .then(res => {
+                        console.log(res.data)
+                        if (res.data.success) {
+                            navigate(location?.state ? location.state : "/")
+                        }
+                    })
             })
             .catch(error => {
                 console.log(error)
